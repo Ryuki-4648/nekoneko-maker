0f4dd2898b6519aea5df83728095e4df765aff9f
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-10-03 23:57:17 +0900

    名前、カラー、turnの値を表示させる

--- neko/src/App.vue
+++ neko/src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const name = ref('');
+const rotate = ref(0);
+const backgroundColor = ref('#fafafa');
 </script>
+<!-- script setup コンポーネントのロジックを設定 -->
 
 <template>
   <header>
@@ -16,29 +22,46 @@ import TheWelcome from './components/TheWelcome.vue'
     <TheWelcome />
     <div>
       <h1>ねこねこメーカー</h1>
-      <h2>自分だけのオリジナルねこちゃんをつくろう。</h2>
+      <h2>自分だけのオリジナルねこをつくろう。</h2>
 
       <img alt="ねこのイラスト" class="image-cat" src="" width="200" height="200" />
       <p v-bind:style="{ display: 'inline-block', backgroundColor: backgroundColor }"></p>
 
-      <p><span v-if="name">なまえは {{ name }} です。</span><span v-else>おなまえを入力してください。</span></p>
-      <p><input type="text" name="name" v-model="name"></p>
+      <p><span v-if="name">おなまえは {{ name }} です。</span><span v-else>おなまえを入力してください。</span></p>
+      <p><input type="text" v-model="name" class="input-name"></p>
       
       <input type="range" name="range" v-model="rotate" min="0" step="0.01" max="1">
       <p>{{ rotate }} turn</p>
 
       <p class="color-picker">カラーピッカー</p>
       <input type="color" name="color" v-model="backgroundColor">
-      <p class="color-code">カラーコードを表示</p>
-      <p>{{ backgroundColor }}</p>
+      <p class="color-code">カラーコード: {{ backgroundColor }}</p>
     </div>
   </main>
 </template>
+<!-- v-model を使用してデータバインディングを行う。 -->
 
 <style scoped>
+h1 {
+  font-size: 24px;
+}
+h2 {
+  font-size: 18px;
+  margin: 0 auto 40px;
+}
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+.input-name {
+  border: 1px solid #dadada;
+  height: 36px;
+  width: 200px;
+  padding: 2px 2px 2px 6px;
+  font-size: 14px;
+}
+.image-cat {
+  margin: 0 auto 40px;
 }
 input[type="color"] {
   border: none;
